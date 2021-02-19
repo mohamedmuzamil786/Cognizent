eb5606435829f3ac6c00235a2c358694f63d9b43
mohamedmuzamil786 <mohamedmuzamil03@gmail.com>
2021-02-19 17:13:37 +0530

    done

---
+++ src/org/cts/Sample.java
@@ -0,0 +1,15 @@
+package org.cts;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("sample1");
+		
+		System.out.println("sample2");
+		System.out.println("sample3");
+	}
+
+}
